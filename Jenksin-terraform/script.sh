5970d4d69f6acf4b3d3d2b4d45bfb31b8dbe183c
Ata Tekeli <tekeliata2000@gmail.com>
2024-09-18 13:10:30 +0300

    revert to original

--- Jenksin-terraform/script.sh
+++ Jenksin-terraform/script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-exec > /var/log/user-data.log 2>&1
+exec > >(tee -i /var/log/user-data.log)
+exec 2>&1
 sudo apt update -y
-sudo apt install software-properties-common -y
+sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
 sudo apt install git -y 
