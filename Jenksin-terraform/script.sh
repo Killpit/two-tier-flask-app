5b10c837f481c5ade066ff4b59963f6898d6d6c3
Ata Tekeli <tekeliata2000@gmail.com>
2024-09-18 13:05:01 +0300

    fix shell script for Jenkins pipeline

--- Jenksin-terraform/script.sh
+++ Jenksin-terraform/script.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-exec > >(tee -i /var/log/user-data.log)
-exec 2>&1
+exec > /var/log/user-data.log 2>&1
 sudo apt update -y
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
 sudo apt install git -y 
